c06a63f0aba473d50d28fdd347b1d5547365452f
Christopher Stock <christopherstock@hotmail.co.uk>
2018-09-23 00:16:13 +0100

    moved transitions to their own component

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,25 @@
       </div>
     </div>
     <div class="container content py-6">
-    <transition :name="transitionName" mode="out-in" @beforeLeave="beforeLeave" @enter="enter" @afterEnter="afterEnter"> 
-      <router-view/>
-    </transition>
+      <transition-page> 
+        <router-view/>
+      </transition-page>
     </div>
     <footer class="bg-green-lighter">
       <div class="container py-6">2018</div>
     </footer>
   </div>
 </template>
+
+<script>
+import TransitionPage from "./transitions/TransitionPage.vue";
+export default {
+  name: "App",
+  components: {
+    TransitionPage
+  }
+};
+</script>
 
 <style>
 @import url("./assets/css/main.css");
@@ -39,80 +49,4 @@
 .content {
   flex: 1;
 }
-.fade-enter-active,
-.fade-leave-active {
-  transition-duration: 0.3s;
-  transition-property: opacity;
-  transition-timing-function: ease;
-}
-
-.fade-enter,
-.fade-leave-active {
-  opacity: 0;
-}
-.slide-left-enter-active,
-.slide-left-leave-active,
-.slide-right-enter-active,
-.slide-right-leave-active {
-  transition-duration: 0.5s;
-  transition-property: height, opacity, transform;
-  transition-timing-function: cubic-bezier(0.55, 0, 0.1, 1);
-  overflow: hidden;
-}
-
-.slide-left-enter,
-.slide-right-leave-active {
-  opacity: 0;
-  transform: translate(2em, 0);
-}
-
-.slide-left-leave-active,
-.slide-right-enter {
-  opacity: 0;
-  transform: translate(-2em, 0);
-}
 </style>
-<script>
-const DEFAULT_TRANSITION = "fade";
-export default {
-  name: "App",
-  data() {
-    return {
-      prevHeight: 0,
-      transitionName: DEFAULT_TRANSITION
-    };
-  },
-  created() {
-    this.$router.beforeEach((to, from, next) => {
-      let transitionName = to.meta.transitionName || from.meta.transitionName;
-
-      if (transitionName === "slide") {
-        const toDepth = to.path.split("/").length;
-        const fromDepth = from.path.split("/").length;
-        transitionName = toDepth < fromDepth ? "slide-right" : "slide-left";
-      }
-
-      this.transitionName = transitionName || DEFAULT_TRANSITION;
-
-      next();
-    });
-  },
-  methods: {
-    beforeLeave(element) {
-      this.prevHeight = getComputedStyle(element).height;
-    },
-    enter(element) {
-      const { height } = getComputedStyle(element);
-
-      element.style.height = this.prevHeight;
-
-      setTimeout(() => {
-        element.style.height = height;
-      });
-    },
-    afterEnter(element) {
-      element.style.height = "auto";
-    }
-  }
-};
-</script>
